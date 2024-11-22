fa32b47aaf79fca98f31dbb1255c5eed27a551d8
maryam <mkhan744@my.centennialcollege.ca>
2024-11-21 22:28:49 -0500

    services page

---
+++ client/src/pages/services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+export default function services() {
+    return (
+      <>
+        <p>Our Services</p>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                    <h4>Cakes</h4>
+                    <hr/>
+                    <h4>Cupcakes</h4>
+                    <hr/>
+                    <h4>specialty desserts</h4>
+                    <hr/>
+                </div>
+      </>
+    );
+  }
