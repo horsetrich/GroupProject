7bec762e0cbb2dd64d0826d80ace00717e3ee3af
horsetrich <matthewkool12@gmail.com>
2024-11-27 23:32:36 -0500

    added email capability to the contact page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import emailjs from '@emailjs/browser';
 
 import Home from './pages/home';
 import About from './pages/about';
--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
+const dotenv = require('dotenv');
+
+
+dotenv.config();
 
     const Contact = () => {
         const form = useRef();
@@ -7,27 +11,63 @@ import emailjs from '@emailjs/browser';
         const sendEmail = (e) => {
           e.preventDefault();
       
-          emailjs.sendForm("service_sq657pn", "template_lnw9r5f", form.current)
-            .then((result) => {
-              console.log(result.text);
-              alert('Message sent successfully!');
-            }, (error) => {
-              console.log(error.text);
-              alert('Failed to send message, please try again.');
-            });
-        };
+        
+            emailjs
+              .sendForm(process.env.EMAILJS_SERVICEID, process.env.EMAIL_TEMPLATE, form.current, {
+                publicKey: process.env.EMAILJS_PUBLICKEY,
+              })
+              .then(
+                () => {
+                  console.log('SUCCESS!');
+                },
+                (error) => {
+                  console.log('FAILED...', error.text);
+                },
+              );
+          };
       
         return (
-          <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" required />
-            <label>Email</label>
-            <input type="email" name="user_email" required />
-            <label>Message</label>
-            <textarea name="message" required />
-            <input type="submit" value="Send" />
+          <form id="myForm" ref={form} onSubmit={sendEmail}>
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input
+                type="name"
+                name="name"
+                class="form-control"
+                id="name"
+                placeholder="enter your name"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input
+                type="email"
+                name="email"
+                class="form-control"
+                id="email"
+                placeholder="enter your email"
+              />
+            </div>
+            <div class="form-group">
+              <label for="subject">Subject</label>
+              <input
+                type="text"
+                name="subject"
+                class="form-control"
+                id="subject"
+                placeholder="enter email subject"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="message">Message</label>
+              <textarea class="form-control" id="message" name="message" rows="5"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary" value="Send">Submit</button>
           </form>
         );
-      };
+    };
 
 export default Contact;
